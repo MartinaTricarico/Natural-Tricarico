3ff5a970d61f6d1fe81bcc82d0dd3725d8b951b8
Martina Tricarico <tricaricomartina4@gmail.com>
2022-02-16 15:05:07 -0300

    firebase II

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,5 +1,5 @@
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { db } from "../Firebase.js";
 import { CartContext } from "../context/CartContext";
 import { Delete } from "@material-ui/icons";
@@ -8,6 +8,14 @@ import { addDoc, collection } from "firebase/firestore";
 
 const Cart = () => {
   const { items, removeItem, totalCart } = useContext(CartContext);
+  const [success, setSuccess] = useState(false);
+  const [orderId, setOrderId] = useState({
+    name: "",
+    lastname: "",
+    address: "",
+    email: "",
+    phone: "",
+  });
 
   const checkout = () => {
     console.log(items);
@@ -31,110 +39,130 @@ const Cart = () => {
     };
 
     const order = { buyers: buyer, items: itemsToBuy, total: 1231 };
-    addDoc(
-      collection(db, "orders")
-        .then((doc) => {
-          console.log("el id de mi orden creada es ", doc.id);
-        })
-        .catch((err) => {
-          console.log("algo malo paso", err);
-        })
-    );
+    addDoc(collection(db, "orders"), order)
+      .then((doc) => {
+        setOrderId(doc.id);
+        setSuccess(true);
+        console.log("el id de mi doc creado es ", doc.id);
+      })
+      .catch((err) => {
+        console.log("algo malo paso", err);
+      });
   };
 
   return (
-    <Container>
-      <Row>
-        <Col xs={{ span: 8, offset: 2 }}>
-          <Card className="panel panel-info">
-            <Card.Header>
-              <Card.Title className="panel-title">
-                <Row>
-                  <Col xs={6}>
-                    <h5>
-                      <span className="glyphicon glyphicon-shopping-cart"></span>
-                      Carrito de compras
-                    </h5>
+    <div>
+      <Container>
+        <Row>
+          <Col xs={{ span: 8, offset: 2 }}>
+            <Card className="panel panel-info">
+              <Card.Header>
+                <Card.Title className="panel-title">
+                  <Row>
+                    <Col xs={6}>
+                      <h5>
+                        <span className="glyphicon glyphicon-shopping-cart"></span>
+                        Carrito de compras
+                      </h5>
+                    </Col>
+                    <Col xs={6}></Col>
+                  </Row>
+                </Card.Title>
+              </Card.Header>
+              <Card.Body>
+                {items.length >= 1 ? (
+                  items.map((item, index) => {
+                    return (
+                      <div key={index}>
+                        <Row>
+                          <Col xs={2}>
+                            <img
+                              style={{ height: "80px" }}
+                              src={item.imageUrl}
+                            />
+                          </Col>
+                          <Col xs={4}>
+                            <h4 className="product-name">
+                              <strong>{item.name}</strong>
+                            </h4>
+                            <h4>
+                              <small> Detalle: {item.details}</small>
+                            </h4>
+                          </Col>
+                          <Col xs={6}>
+                            <Row>
+                              <Col xs={6} className="text-right">
+                                <h6>
+                                  <strong>${item.price} /kg</strong>
+                                </h6>
+                              </Col>
+                              <Col xs={4}>
+                                <input
+                                  type="text"
+                                  className="form-control input-sm"
+                                  value={item.qty}
+                                />
+                              </Col>
+                              <Col xs={2}>
+                                <Button
+                                  xs={2}
+                                  variant="primary"
+                                  variant="danger"
+                                  onClick={() => removeItem(item.id)}
+                                >
+                                  <Delete />
+                                </Button>
+                              </Col>
+                            </Row>
+                          </Col>
+                        </Row>
+                        <hr />
+                      </div>
+                    );
+                  })
+                ) : (
+                  <div className="">
+                    <p>No hay items</p>
+                    <Link to="/">
+                      <Button>Continuar comprando</Button>
+                    </Link>
+                  </div>
+                )}
+              </Card.Body>
+              <Card.Footer>
+                <Row className="text-center">
+                  <Col xs={9}>
+                    <h4 className="text-right">
+                      Total <strong>$ {totalCart}</strong>
+                    </h4>
                   </Col>
-                  <Col xs={6}></Col>
+                  <Col xs={3}>
+                    <Button variant="success" onClick={checkout}>
+                      Terminar compra
+                    </Button>
+                  </Col>
                 </Row>
-              </Card.Title>
-            </Card.Header>
-            <Card.Body>
-              {items.length >= 1 ? (
-                items.map((item, index) => {
-                  return (
-                    <div key={index}>
-                      <Row>
-                        <Col xs={2}>
-                          <img style={{ height: "80px" }} src={item.imageUrl} />
-                        </Col>
-                        <Col xs={4}>
-                          <h4 className="product-name">
-                            <strong>{item.name}</strong>
-                          </h4>
-                          <h4>
-                            <small> Detalle: {item.details}</small>
-                          </h4>
-                        </Col>
-                        <Col xs={6}>
-                          <Row>
-                            <Col xs={6} className="text-right">
-                              <h6>
-                                <strong>${item.price} /kg</strong>
-                              </h6>
-                            </Col>
-                            <Col xs={4}>
-                              <input
-                                type="text"
-                                className="form-control input-sm"
-                                value={item.qty}
-                              />
-                            </Col>
-                            <Col xs={2}>
-                              <Button
-                                xs={2}
-                                variant="primary"
-                                variant="danger"
-                                onClick={() => removeItem(item.id)}
-                              >
-                                <Delete />
-                              </Button>
-                            </Col>
-                          </Row>
-                        </Col>
-                      </Row>
-                      <hr />
-                    </div>
-                  );
-                })
-              ) : (
-                <div className="">
-                  <p>No hay items</p>
-                  <Link to="/">
-                    <Button>Continuar comprando</Button>
-                  </Link>
-                </div>
-              )}
-            </Card.Body>
-            <Card.Footer>
-              <Row className="text-center">
-                <Col xs={9}>
-                  <h4 className="text-right">
-                    Total <strong>$ {totalCart}</strong>
-                  </h4>
-                </Col>
-                <Col xs={3}>
-                  <Button variant="success" onClick={checkout}>
-                    Terminar compra
-                  </Button>
-                </Col>
-              </Row>
-            </Card.Footer>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
+              </Card.Footer>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+
+      {success ? (
+        <div
+          style={{
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+            background: "rgba(0,0,0,0.7)",
+          }}
+        >
+          <h1>Tu compra se ha realizado con exito</h1>
+          <p>Puedes hacer el seguimiento con el siguiente id {orderId}</p>
+        </div>
+      ) : null}
+    </div>
   );
 };
 export default Cart;
